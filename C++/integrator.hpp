6be0b592e1cfd21ae147bb26ef6ce1895308b810
Adam Jermyn <adamjermyn@gmail.com>
2017-04-28 19:46:49 +0100

    Minor formatting changes.

--- C++/integrator.hpp
+++ C++/integrator.hpp
@@ -11,15 +11,15 @@
 
 const double pi = 3.141592653589;
 const double fourierCoeff = 1;			 	// Accounts for the integrals being evaluated
-											// in Fourier space. Careful derivation shows this to be
-											// 1, but it is most easily seen by considering the
-											// form of the delta function in a finite volume.
+							// in Fourier space. Careful derivation shows this to be
+							// 1, but it is most easily seen by considering the
+							// form of the delta function in a finite volume.
 const double unitConv = 1/(2*pi);			// This puts things in units of the mixing length.
-											// The integration internally uses units of the mixing
-											// wavevector, meaning that the lengths are all in units
-											// of 1/k = L_mix/2pi. To fix this, we divide the position and
-											// velocity correlators by 2pi to the power of the number of factors
-											// of length, so they are in units of L_mix.
+							// The integration internally uses units of the mixing
+							// wavevector, meaning that the lengths are all in units
+							// of 1/k = L_mix/2pi. To fix this, we divide the position and
+							// velocity correlators by 2pi to the power of the number of factors
+							// of length, so they are in units of L_mix.
 
 // -------------------------------------
 // Functions
