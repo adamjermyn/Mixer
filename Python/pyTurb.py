2d3799d8d6ff7ae3e1b32bfc8acbc464fbe7d57e
Adam Jermyn <adamjermyn@gmail.com>
2017-04-28 19:40:52 +0100

    Moved files around.

--- Python/pyTurb.py
+++ Python/pyTurb.py
@@ -2,17 +2,17 @@ import numpy as np
 import ctypes
 
 _turb = ctypes.CDLL('/home/asj42/Documents/Turbulence/Build/turb.so')
-_turb.coeffs3.argtypes = [ctypes.c_double for _ in range(12)]
+_turb.coeffs3.argtypes = [ctypes.c_double for _ in range(12)] + [ctypes.c_int]
 _turb.coeffs3.restype = ctypes.POINTER(ctypes.c_double)
-_turb.coeffs2.argtypes = [ctypes.c_double for _ in range(8)]
+_turb.coeffs2.argtypes = [ctypes.c_double for _ in range(8)] + [ctypes.c_int]
 _turb.coeffs2.restype = ctypes.POINTER(ctypes.c_double)
 
-def coeffs2(omega, w, tW, tS, tP, N2, tolr, tola):
+def coeffs2(omega, w, tW, tS, tP, N2, tolr, tola, maxEval):
 	global _turb
 
 	res = _turb.coeffs2(ctypes.c_double(tolr),ctypes.c_double(tola),\
 			ctypes.c_double(omega),ctypes.c_double(w),ctypes.c_double(tW),\
-			ctypes.c_double(tS),ctypes.c_double(tP),ctypes.c_double(N2))
+			ctypes.c_double(tS),ctypes.c_double(tP),ctypes.c_double(N2),ctypes.c_int(maxEval))
 
 	ret = np.zeros((7,7,2))
 	for i in range(7):
@@ -22,14 +22,14 @@ def coeffs2(omega, w, tW, tS, tP, N2, tolr, tola):
 
 	return ret
 
-def coeffs3(B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola):
+def coeffs3(B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval):
 	global _turb
 
 	res = _turb.coeffs3(ctypes.c_double(tolr),ctypes.c_double(tola),\
 			ctypes.c_double(B),ctypes.c_double(tB),ctypes.c_double(pB),\
 			ctypes.c_double(omega),ctypes.c_double(w),ctypes.c_double(tW),\
 			ctypes.c_double(tS),ctypes.c_double(tP),ctypes.c_double(N2),\
-			ctypes.c_double(chi))
+			ctypes.c_double(chi),ctypes.c_int(maxEval))
 
 	ret = np.zeros((7,7,2))
 	for i in range(7):
@@ -66,7 +66,7 @@ def coeffs(params, spherical=True):
 	the returned quantities are in spherical coordinates, otherwise they
 	are in cylindrical coordinates. Note that errors are not transformed.
 	'''
-	if len(params) == 8:
+	if len(params) == 9:
 		r = coeffs2(*params)
 		theta = params[4]
 		if spherical:
@@ -74,7 +74,7 @@ def coeffs(params, spherical=True):
 			r[4:,:3,0] = cylindricalToSphericalTensor(r[4:,:3,0], theta)
 			r[4:,4:,0] = cylindricalToSphericalTensor(r[4:,4:,0], theta)
 			r[:3,4:,0] = cylindricalToSphericalTensor(r[:3,4:,0], theta)
-	elif len(params) == 12:
+	elif len(params) == 13:
 		r = coeffs3(*params)
 		theta = params[7]
 		if spherical:
