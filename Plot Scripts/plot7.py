fd38232f2ceb003a7b4339ce5a19d802321a739f
Adam Jermyn <adamjermyn@gmail.com>
2017-04-29 10:41:03 +0100

    Fixed legend placement on plot7.

--- Plot Scripts/plot7.py
+++ Plot Scripts/plot7.py
@@ -20,10 +20,9 @@ x, y = delta, r[...,5,4,0]
 plt.plot(x,y, label='$v_r v_\\theta$')
 x, y = delta, r[...,1,4,0]
 plt.plot(x,y, label='$v_r r_\\theta$')
-plt.ylim([-0.04,0.03])
 plt.xlabel('$\delta$')
 plt.ylabel('(Mixing Units)')
-plt.legend(loc='lower right')
+plt.legend(loc='upper left')
 plt.tight_layout()
 
 plt.subplot(121)
@@ -33,8 +32,7 @@ plt.plot(x,y, label='$v_r v_r$')
 x, y = delta, r[...,0,4,0]
 plt.plot(x,y, label='$v_r r_r$')
 plt.xlabel('$\delta$')
-plt.ylim([-0.10,0.25])
-plt.legend(loc='lower right')
+plt.legend(loc='upper left')
 plt.tight_layout()
 
 plt.savefig('Plots/Plot7.pdf')
