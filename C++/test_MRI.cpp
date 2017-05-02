edb160150cd7447d1370d09037d63e7d094484b0
Adam Jermyn <adamjermyn@gmail.com>
2017-05-02 21:14:50 +0100

    Added further MRI test.

--- C++/test_MRI.cpp
+++ C++/test_MRI.cpp
@@ -22,6 +22,9 @@ using namespace std;
 // -------------------------------------
 
 TEST_CASE("MRI Dispersion Relation","[MRI]") {
+	// Tests to see whether the eigenvalues of the time evolution matrix match the analytic
+	// dispersion relation for the MRI.
+
 	double tol = 3*eps;
 	double pi = 3.14159265358979;
 
@@ -51,10 +54,65 @@ TEST_CASE("MRI Dispersion Relation","[MRI]") {
 		// Align k with z
 		f.set_k(1,0,0);
 
-		for (int i=0;i<5;i++) {
+		// Test eigenvalues against analytic dispersion relation
+		for (int i=0;i<4;i++) {
 			cdouble x = f.eigvals(i,i);
 			REQUIRE(abs(x*(pow(x,4) + (pow(omegaa,2)+2*pow(f.kva,2))*pow(x,2)+pow(f.kva,4) - 3*pow(omegaa*f.kva,2)))
 			 <=5*tol*(1 +pow(abs(x),5)+pow(omegaa,2)*pow(f.kva,2)+pow(f.kva,4)));
+		}
+
+	}
+
+}
+
+
+TEST_CASE("MRI Dispersion Relation Invariance","[MRI-Invariance]") {
+	// Tests to see if letting w -> -w and tW -> tW + Pi changes anything. It shouldn't.
+
+	double tol = 3*eps;
+	double pi = 3.14159265358979;
+
+	// B is along z-hat
+	double B;
+	double tB = 0;
+	double pB = 0;
+
+	// gradW = -3*omega/2
+	double omegaa;
+	double w;
+	double tW = pi/2;
+
+	// Entropy gradient and pressure gradient along z-hat, stable
+	double tS = 0;
+	double tP = 0;
+	double N22 = 0;
+
+	// Wavevector
+	double k;
+	double kT;
+	double kP;
+
+	for (int j=0;j<100;j++) {
+		B = 10*static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+		omegaa = 10*static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+		w = -3*omegaa/2;
+
+		// Construct matrices
+		flmatrix f1 = flmatrix(B,tB,pB,w,tW,tS,tP,N22,omegaa);
+		flmatrix f2 = flmatrix(B,tB,pB,-w,3*tW,tS,tP,N22,omegaa);
+
+		// Random k
+		k = 10*static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+		kT = 3.141*static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+		kP = 2*3.141*static_cast <double> (rand()) / static_cast <double> (RAND_MAX);
+
+		f1.set_k(k, kT, kP);
+		f2.set_k(k, kT, kP);
+
+		for (int i=0;i<4;i++) {
+			for (int k=0;k<4;k++) {
+				REQUIRE(abs(f1.m(i,k) - f2.m(i,k)) < tol);
+			}
 		}
 
 	}
