e19ca0b12defa173d5e1afde5953cf06378eb139
Adam Jermyn <adamjermyn@gmail.com>
2017-05-02 20:52:29 +0100

    Removed chi reference from test script.

--- Plot Scripts/accretionTest.py
+++ Plot Scripts/accretionTest.py
@@ -21,7 +21,6 @@ tW = np.pi/2
 omega = 1.
 w = -3./2
 N2 = 0
-chi = 0
 tolr = 1e-10
 tola = 1e-10
 maxEval = 100000
@@ -31,7 +30,7 @@ B = 0.1
 w = 1.
 tW = np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -39,7 +38,7 @@ print(r[3:,3:,0])
 w = -1.
 tW = 3*np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -47,7 +46,7 @@ print(r[3:,3:,0])
 w = -1.
 tW = np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -55,7 +54,7 @@ print(r[3:,3:,0])
 w = 1.
 tW = 3*np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -63,7 +62,7 @@ print(r[3:,3:,0])
 w = 10.05
 tW = np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -71,7 +70,7 @@ print(r[3:,3:,0])
 w = -10.05
 tW = 3*np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -79,7 +78,7 @@ print(r[3:,3:,0])
 w = -10.05
 tW = np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
@@ -87,7 +86,7 @@ print(r[3:,3:,0])
 w = 10.05
 tW = 3*np.pi/2
 
-params = (B, tB, pB, omega, w, tW, tS, tP, N2, chi, tolr, tola, maxEval)
+params = (B, tB, pB, omega, w, tW, tS, tP, N2, tolr, tola, maxEval)
 r = coeffs(params)
 
 print(r[3:,3:,0])
