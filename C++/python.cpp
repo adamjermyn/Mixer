2f7efcc13bb468c736c03561baf32bc5b680da51
Adam Jermyn <adamjermyn@gmail.com>
2017-05-12 10:11:51 +0100

    Updated python interface.

--- C++/python.cpp
+++ C++/python.cpp
@@ -49,8 +49,8 @@ extern "C" double* coeffs2(double tolr, double tola, double omega, double w, dou
 	double pB = 0;
 
 	const unsigned intDim = 2;
-	const double transform_aa[3] = {0,0};
-	const double transform_bb[3] = {pi,2*pi};
+	const double transform_aa[2] = {0,0};
+	const double transform_bb[2] = {pi,2*pi};
 
 	double ret[correlDim*correlDim];
 	double err[correlDim*correlDim];
@@ -98,8 +98,8 @@ extern "C" double* coeffs2spherical(double theta, double tolr, double tola, doub
 	double pB = 0;
 
 	const unsigned intDim = 2;
-	const double transform_aa[3] = {0,0};
-	const double transform_bb[3] = {pi,2*pi};
+	const double transform_aa[2] = {0,0};
+	const double transform_bb[2] = {pi,2*pi};
 
 	double ret[correlDim*correlDim];
 	double err[correlDim*correlDim];
@@ -149,8 +149,8 @@ extern "C" double* coeffs2sphericalSpecific(int output[correlDim*correlDim], dou
 	double pB = 0;
 
 	const unsigned intDim = 2;
-	const double transform_aa[3] = {0,0};
-	const double transform_bb[3] = {pi,2*pi};
+	const double transform_aa[2] = {0,0};
+	const double transform_bb[2] = {pi,2*pi};
 
 	double ret[correlDim*correlDim];
 	double err[correlDim*correlDim];
@@ -168,4 +168,64 @@ extern "C" double* coeffs2sphericalSpecific(int output[correlDim*correlDim], dou
 	}
 
 	return net;
+}
+
+extern "C" double* coeffs2sphericalSpecificBox(double mins[2], double maxs[2], int output[correlDim*correlDim], double theta, double tolr, double tola, double omega, double w, double tW, double tS, double tP, double N2, int maxEval) {
+
+	double B = 0;
+	double tB = 0;
+	double pB = 0;
+
+	const unsigned intDim = 2;
+
+	double ret[correlDim*correlDim];
+	double err[correlDim*correlDim];
+	double* net = new double[2*correlDim*correlDim];
+	
+	flmatrix f(B,tB,pB,w,tW,tS,tP,N2,omega);
+	f.theta = theta;
+	f.output = output;
+	integral(correlDim*correlDim,&F,&f,intDim, mins, maxs, maxEval, tolr, tola,ERROR_INDIVIDUAL,
+			ret,err);
+
+	for (int i=0;i<correlDim*correlDim;i++) {
+		net[2*i] = ret[i];
+		net[2*i + 1] = err[i];
+	}
+
+	return net;
+}
+
+extern "C" double* coeffs3sphericalSpecificBox(double mins[3], double maxs[3], int output[correlDim*correlDim], double theta, double tolr, double tola, double B, double tB, double pB, double omega, double w, double tW, double tS, double tP, double N2, int maxEval) {
+
+	const unsigned intDim = 3;
+
+	double ret[correlDim*correlDim];
+	double err[correlDim*correlDim];
+	double* net = new double[2*correlDim*correlDim];
+	
+	flmatrix f(B,tB,pB,w,tW,tS,tP,N2,omega);
+	f.theta = theta;
+	f.output = output;
+	integral(correlDim*correlDim, &F, &f, intDim, mins, maxs,maxEval, tolr, tola ,ERROR_INDIVIDUAL,
+			ret,err);
+
+	for (int i=0;i<correlDim*correlDim;i++) {
+		net[2*i] = ret[i];
+		net[2*i + 1] = err[i];
+	}
+
+	return net;
+}
+
+extern "C" double* correlator(double k, double kT, double kP, double B, double tB, double pB, double omega, double w, double tW, double tS, double tP, double N2) {
+	flmatrix f(B,tB,pB,w,tW,tS,tP,N2,omega);
+	f.set_k(k, kT, kP);
+	double* ret = new double[dim*dim];
+	for (int i=0;i<dim;i++) {
+		for (int j=0;j<dim;j++) {
+			ret[dim*i + j] = f.correlator(i,j);
+		}
+	}
+	return ret;
 }
