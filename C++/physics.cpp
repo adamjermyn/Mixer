c8db0849a2d33d735dcbae25e20223e77b907f97
Adam Jermyn <adamjermyn@gmail.com>
2017-05-17 12:03:34 -0700

    Bug fix.

--- C++/physics.cpp
+++ C++/physics.cpp
@@ -77,8 +77,8 @@ Matrix1 flmatrix::derivative(int i) {
 	ret(3,1) = 0;
 	for (int j=0;j<=i;j++) {
 		int k = i - j;
-		ret(3,1) -= nCr(n, j) * 2*omega*wmag*ba.db[j][0]*dot(ba.db[k], ba.wHat);
-		ret(3,1) -= nCr(n, j) * N2*dot(ba.db[j], entHat)*dot(ba.db[k], presHat);
+		ret(3,1) -= nCr(i, j) * 2*omega*wmag*ba.db[j][0]*dot(ba.db[k], ba.wHat);
+		ret(3,1) -= nCr(i, j) * N2*dot(ba.db[j], entHat)*dot(ba.db[k], presHat);
 	}
 
 	ret(2,3) = -2*omega*dot(ba.a, ba.de[i]);
