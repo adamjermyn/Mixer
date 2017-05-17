6987c176c576e482bb965730b44542cf888733aa
Adam Jermyn <adamjermyn@gmail.com>
2017-05-16 23:43:37 -0700

    Fixed coordinate bug.

--- C++/physics.cpp
+++ C++/physics.cpp
@@ -74,7 +74,7 @@ void flmatrix::set_Mdot() {
 	mdot(2,3) = -2*omega*dot(ba.a, ba.de);
 	mdot(3,2) = -mdot(2,3);
 
-	mdot *= wmag*ba.kHat[2];
+	mdot *= wmag*ba.kHat[1];
 }
 
 void flmatrix::compute_eigensystem() {
