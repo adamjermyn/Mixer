Fixed KR signs.

Plot Scripts/plot1.py
@@ -40,7 +40,7 @@ plt.loglog(x,y,label='$v_r v_r$')
 x, y = omega[(omega>=0.5) & (omega<=1000)], r[(omega>=0.5) & (omega<=1000)][...,0,3,0]
 plt.xlim([0,max(omega)])
 plt.loglog(x,y,label='$v_r r_r$')
-plt.loglog(x, (1./3)*np.abs(flux(np.pi/4, x, '14', 'Sr')), label='$v_r r_r$ *')
+plt.loglog(x, -(1./3)*np.abs(flux(np.pi/4, x, '14', 'Sr')), label='$v_r r_r$ *')
 plt.xlabel('$ \Omega/|N|$')
 plt.legend(loc='upper right')
 

Plot Scripts/plot3.py
@@ -29,10 +29,10 @@ y = np.abs(y)
 plt.loglog(x,y,label='$v_r r_\phi$')
 
 # KR comparison
-plt.plot(x[x<1], x[x<1]*flux(np.pi/4, x[x<1], '13', 'zero'), label='$v_r r_r$ *')
+plt.plot(x[x<1], -x[x<1]*flux(np.pi/4, x[x<1], '13', 'zero'), label='$v_r r_r$ *')
 
 
-plt.loglog(x[x<1], np.abs(paperOne(x[x<1])), label='$v_r v_\phi$ (*)')
+plt.loglog(x[x<1], np.abs(paperOne(x[x<1])), label='$v_r v_\phi$ **')
 plt.xlabel('$ \Omega/|N|$')
 plt.ylabel('(Mixing Units)')
 plt.legend(loc='upper right')

Plot Scripts/plot7.py
@@ -34,7 +34,7 @@ plt.plot(x,y, label='$v_r v_r$')
 x, y = delta, r[...,0,3,0]
 plt.plot(x,y, label='$v_r r_r$')
 print(flux(np.pi/2,1e-5,'14','Sr'))
-plt.plot(x, (1./3)*(flux(np.pi/2, 1e-4, '14', 'Sr')*np.cos(x) + np.sin(x)*flux(np.pi/2, 1e-4, '14', 'St')), label='$v_r r_r$ *')
+plt.plot(x, -(1./3)*(flux(np.pi/2, 1e-4, '14', 'Sr')*np.cos(x) + np.sin(x)*flux(np.pi/2, 1e-4, '14', 'St')), label='$v_r r_r$ *')
 plt.xlabel('$\delta$')
 plt.ylabel('(Mixing Units)')
 plt.legend(loc='upper left')